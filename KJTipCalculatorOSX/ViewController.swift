25e89ab6b572da780c45684c524abbad7b77f152
Kristopher Johnson <kris@kristopherjohnson.net>
2015-11-11 06:31:37 -0500

    Initial Commit

---
+++ KJTipCalculatorOSX/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  KJTipCalculatorOSX
+//
+//  Created by Kristopher Johnson on 11/11/15.
+//  Copyright © 2015 Kristopher Johnson. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
